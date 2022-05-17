73bb22a547b6d91657b2358cd32dadf27d5fe5e1
patrickpoli <patrickpolicarpio08@gmail.com>
2022-05-17 23:51:20 +0800

    Added my account page

---
+++ client/my-account.php
@@ -0,0 +1,19 @@
+<?php 
+require_once './components/header.layout.php'; 
+?>
+
+<div class="container">
+  <div class="section-myaccount page-content-padding">
+    <h1>
+      MY ORDERS
+    </h1>
+
+    <br /><br /><br />
+
+    <p>
+      You have no orders yet, click <a href="/projects/rrperfumes/client/browse-products">here</a> to shop now
+    </p>
+  </div>
+</div>
+
+<?php require './components/footer.layout.php'; ?>
