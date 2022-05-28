caa97a31b7dba2cda08d7d89989ea2af2cb3af89
patrickpoli <patrickpolicarpio08@gmail.com>
2022-05-28 23:14:27 +0800

    track order page

---
+++ client/pay-order.php
@@ -0,0 +1,19 @@
+<?php 
+require_once './components/header.layout.php'; 
+?>
+
+<div class="container">
+  <div class="section-myaccount page-content-padding">
+    <h1>
+      PAY MY ORDER
+    </h1>
+
+    <br /><br /><br />
+
+    <p>
+      
+    </p>
+  </div>
+</div>
+
+<?php require './components/footer.layout.php'; ?>
--- client/track-order.php
+++ client/track-order.php
@@ -4,11 +4,24 @@ require_once './components/header.layout.php';
 ?>
 
 <div class="page-content">
-  <div class="banner-container about-us-banner">  
-    
-  </div>
-
   <div class="container page-content-padding">
-    
+    <div class="section-track-order">
+      <h1>
+         TRACK ORDER
+      </h1>
+
+      <div class="input-container">
+        <form>
+          <input type="text" placeholder="Enter order reference number" required />
+          <button type="submit" class="btn">FIND MY ORDER</button>
+        </form>
+      </div>
+
+      <div class="order-data-container">
+        <h3>
+          No order data yet
+        </h3>
+      </div>
+    </div>
   </div>
 </div>
